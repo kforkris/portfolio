af5e60cdeb4af7a45ef3361a50b53fd7c0c5b267
kfor.krish <itskrishnaojha@gmail.com>
2025-02-12 23:37:54 +0530

    Added ink

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -53,7 +53,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1UWU9Xizs_y44DbTqBeDrhzE1dEs7JOli/view?usp=sharing"
+              href="https://drive.google.com/file/d/1mkLsBIyJst1WBdDtYAqyUktaOSFvLBdT/view?usp=drive_link"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
